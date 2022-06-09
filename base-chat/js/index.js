d6e6c368b9b2c7ecfd346afcfe0b6ed43c3244a8
Vinicius  Prates <89698942+vinicius-prates@users.noreply.github.com>
2022-06-09 13:35:20 -0300

    Update no JS para o codigo mais atualizado.

--- base-chat/js/index.js
+++ base-chat/js/index.js
@@ -4,18 +4,26 @@ input.addEventListener("keypress", function (event) {
         enviarMSG();
     }
 });
+
+//Botao enviarMSG
+var botao = document.getElementById("botao1");
+botao.addEventListener("click", function() {
+    enviarMSG();
+})
+
 //CONTENT DELIVERY NETWORK - CDN
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 import { getDatabase, ref, set, push, remove, onValue } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC7XYJp2g2OedBAM360JGwWn6SPNYIpajs",
-    authDomain: "chat-prates-baltazar.firebaseapp.com",
-    projectId: "chat-prates-baltazar",
-    storageBucket: "chat-prates-baltazar.appspot.com",
-    messagingSenderId: "739330437586",
-    appId: "1:739330437586:web:079c7142885cede95d980b",
-    measurementId: "G-DNPLMZPK2F"
+    apiKey: "AIzaSyAQ8kWC_aOjwONPiFUmVqSRvxIQTWU5_x0",
+    authDomain: "chat-balta.firebaseapp.com",
+    databaseURL: "https://chat-balta-default-rtdb.firebaseio.com",
+    projectId: "chat-balta",
+    storageBucket: "chat-balta.appspot.com",
+    messagingSenderId: "973840611745",
+    appId: "1:973840611745:web:bc7b5fe4b5febb6c4833e0",
+    measurementId: "G-20K1D3W2BV"
 
 };
 
@@ -23,7 +31,7 @@ const app = initializeApp(firebaseConfig);
 
 var db = getDatabase(app);
 const dbRef = ref(db, 'exemplo');
-const dbTurma2 = ref(db, 'chat');
+const dbTurma2 = ref(db, 'turma4');
 
 
 var meuhtml = "";
@@ -32,19 +40,47 @@ var nomeUsuario = prompt("Digite seu nome");
 
 onValue(dbRef, (snapshot) => {
     const data = snapshot.val();
-    console.log(data);
+    // console.log(data);
     meuhtml = "";
     snapshot.forEach(function (childSnapshot) {
-        var key = childSnapshot.key;
-        console.log(key);
-        console.log(childSnapshot.val().nome);
-        console.log(childSnapshot.val().mensagem);
+        //utilizar a key pra deletar mensagem
+        let key = childSnapshot.key;
+        // console.log("CHAVE" + key);
+        // console.log("NOME" + childSnapshot.val().nome);
+        // console.log("MSG" + childSnapshot.val().mensagem);
 
-        meuhtml += '<div class="msg"><b>' + childSnapshot.val().nome +  '</i></b><span>' + childSnapshot.val().mensagem + '</span></div>';
+        if(nomeUsuario == childSnapshot.val().nome){
+            meuhtml += '<div class="msg"><div class="self"><div class="eu"><b>' + childSnapshot.val().nome +  '</i></b></i> <span><p class="texto">'
+            + childSnapshot.val().mensagem+'</p><input type="submit" class="deletar" value="Deletar" id="'+`${key}`+'" />'  
+            +"<br>"+ childSnapshot.val().horario + '</span></div></div></div></div>';
+            atualizarHTML();
+            console.log(childSnapshot.key)
+            setTimeout(() => {
+                document.getElementById(key).onclick = (() => {
+                    removerDb(key)
+                })
+            }, 500)
+        }else{
+            meuhtml += '<div class="msg"><div class="conteudo"><div class="outros"><b>' + childSnapshot.val().nome +  
+            '</i></b></i> <span>'+"<br>"+'<div class="mensagem"><p class="texto2">' + childSnapshot.val().mensagem +'</p></div>'+ "<br>"
+            + '<div class="hora">'+ childSnapshot.val().horario + '</div>' + '</span></div></div></div></div>';
+            atualizarHTML();
+        }
+
 
     });
-    atualizarHTML();
 });
+
+function remover(key){
+
+    document.getElementById("key").value = key;
+    removerDb();
+}
+
+function removerDb(key) {
+    var itemKey = document.getElementById("key").value;
+    remove(ref(db, 'exemplo/' + key));
+}
 
 function enviarMSG() {
 
@@ -58,7 +94,7 @@ function enviarMSG() {
         mensagem: document.getElementById("msg").value
     });
     
-    push(ref(db, 'chat'), {
+    push(ref(db, 'turma4'), {
         nome: nomeUsuario,
         horario: hora,
         data: datahj,
@@ -74,7 +110,7 @@ function atualizarHTML() {
 }
 
 function ajustarScroll() {
-    console.log("corrirgir scroll");
+    // console.log("corrirgir scroll");
     var divConteudo = document.getElementById("conteudo");
     divConteudo.scrollTop = divConteudo.scrollHeight;
-}               
+}       
