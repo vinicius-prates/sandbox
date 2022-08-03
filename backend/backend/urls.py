9cd4e1ca05005c729daa9e1e090a3b636cbe80e9
Viktor Marinho <viktormpcs@gmail.com>
2022-08-03 11:28:35 -0300

    Abstract api urls away from the main url file

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 
-router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('api/', include('api.urls')),
     path("admin/", admin.site.urls),
 ]
