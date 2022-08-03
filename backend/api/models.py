f00ba0c3a3f516f5944dbd256e71e30fbaa22869
viktormp <viktormpcs@gmail.com>
2022-08-02 21:35:25 -0300

    removing .idea folders + create api app

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    nome = models.CharField(max_length=255)
+    edv = models.CharField(max_length=20)
+    senha = models.CharField(max_length=255)  # store a hash obv
+    email = models.EmailField()
+    # turma = models.ForeignKey(Turma) ?
+    # perm = models.CharField ?
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path("admin/", admin.site.urls),
 ]
