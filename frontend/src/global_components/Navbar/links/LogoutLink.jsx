0ca761d14d54bbe0f272cc715babd0936a26692c
iguoliveira <igor.or042@gmail.com>
2022-08-05 11:29:25 -0300

    Added hover in lonks + Added interactable logout button

--- frontend/src/global_components/Navbar/links/LogoutLink.jsx
+++ frontend/src/global_components/Navbar/links/LogoutLink.jsx
@@ -1,7 +1,15 @@
-import LogoutIcon from '../assets/logout-icon.png'
+import styled from "styled-components";
+import LogoutIcon from "../assets/logout-icon.png";
 
 export const LogoutLink = () => {
-    return(
-        <img src={ LogoutIcon } />
-    )
-}
+  return (
+    <Logout>
+      <img src={LogoutIcon} />
+    </Logout>
+  );
+};
+
+const Logout = styled.div`
+  line-height: 0;
+  cursor: pointer;
+`;
