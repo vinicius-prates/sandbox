ec55fea1e633c53ee9c1b1d25d2a09df1de242b5
iguoliveira <igor.or042@gmail.com>
2022-08-09 11:28:49 -0300

    Begin with list container

--- frontend/src/pages/justify_manage/JustifyManage.jsx
+++ frontend/src/pages/justify_manage/JustifyManage.jsx
@@ -1,17 +1,36 @@
-import styled from 'styled-components'
-import { Navbar } from '../../global_components/Navbar/Navbar'
-import { InputsContainer } from './components/InputsContainer'
+import styled from "styled-components";
+import { Navbar } from "../../global_components/Navbar/Navbar";
+import { InputsContainer } from "./components/InputsContainer";
+import { ListContainer } from "./components/ListContainer";
 
 export const JustifyManage = () => {
-    return (
-        <Whole>
-            <Navbar admin={true} />
-            <InputsContainer />
-        </Whole>
-    )
-}
+  return (
+    <>
+      <Navbar admin={true} />
+      <Whole>
+        <InputsContainer />
+        <div className="ListContainerBlock">
+        <ListContainer display={true} />
+        <ListContainer display={false} />
+        </div>
+      </Whole>
+    </>
+  );
+};
 
 const Whole = styled.div`
-    height: 100vh;
-    background-color: #EFF1F2;
-`
+  height: 100vh;
+  background-color: #eff1f2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .ListContainerBlock{
+    margin-top: 40px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    gap: 100px;
+  }
+`;
