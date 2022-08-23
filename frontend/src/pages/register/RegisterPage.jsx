92f5ba0b640aa5de2dd5e9a680edebeaed3e1a56
vinicius-prates <viniprates2211@gmail.com>
2022-08-23 09:08:33 -0300

    adding the inputs, the button and the select box to the register page

--- frontend/src/pages/register/RegisterPage.jsx
+++ frontend/src/pages/register/RegisterPage.jsx
@@ -4,8 +4,22 @@ import { getSession } from "../../session";
 import styled from "styled-components";
 import Notiflix from "notiflix";
 import { Navbar } from "../../global_components/Navbar/Navbar";
+import { FormHeader } from "../../global_components/forms/header/FormHeader";
+import { InputText } from "../../global_components/inputs/input_text/InputText";
+import { useState } from "react";
+import { SelectBox } from "../../global_components/inputs/select_box/SelectBox";
+import { Button } from "../../global_components/inputs/button/Button";
 
 export const RegisterPage = () => {
+    
+    const [nome, setNome] = useState("");
+    const [edv, setEdv] = useState("");
+    const [password, setPass ] = useState("");
+    const [confirmPass, setConfPass] = useState("");
+    const [email, setEmail] = useState(""); 
+
+
+
 
     const navigate = useNavigate();
 
@@ -26,7 +40,70 @@ export const RegisterPage = () => {
   
 
     return (
+        <>
         <Navbar/>
+        <Content>
+        <RegisterForm>
+            <FormHeader name="REGISTRO"/>
+            <RegisterInputs>
+                <InputText
+                placeholder="Nome Completo"
+                value={nome}
+                onchange={(evt) => setNome(evt.target.value)}
+                type="text"/>
+
+                <InputText
+                placeholder="EDV"
+                value={edv}
+                onchange={(evt) => setEdv(evt.target.value)}
+                type="text"/>
+                
+                <InputText 
+                placeholder="Senha"
+                value={password}
+                onchange={(evt) => setPass(evt.target.value)}
+                type="password"/>
+
+                <InputText
+                placeholder="Confirmar a Senha"
+                value={confirmPass}
+                onchange={(evt) => setConfPass(evt.target.value)}
+                type="password"/>
+
+                <InputText
+                placeholder="Email"
+                value={email}
+                type="email"/>
+                <SelectBox/>
+                <SelectBox/>
+
+            </RegisterInputs>
+            <Button name="Cadastrar"/>
+        </RegisterForm>
+        </Content>
+        </>
         
     )
 }
+
+const Content = styled.div `
+    height: 100vh;
+    background-color: #eff1f2;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+
+`
+const RegisterForm = styled.div`
+    background-color: #ffffff;
+    padding: 50px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+const RegisterInputs = styled.div`
+
+`
