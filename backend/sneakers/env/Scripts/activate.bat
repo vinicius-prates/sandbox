92dcfd05acef2bae4a9ba5ad447ca35786e04f5d
vinicius-prates <viniprates2211@gmail.com>
2022-08-31 15:43:50 -0300

    adding requirements.txt

--- backend/sneakers/env/Scripts/activate.bat
+++ backend/sneakers/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\CT67CA\Desktop\zimbas\backend\sneakers\env
+set VIRTUAL_ENV=C:\Users\ct67ca\Desktop\sneakerstore\backend\sneakers\env
 
 if not defined PROMPT set PROMPT=$P$G
 
