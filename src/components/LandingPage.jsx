68f53e667080c9e0141aeb7753ee39ed5864cf24
vinicius-prates <viniprates2211@gmail.com>
2022-09-12 16:30:47 -0300

    creating the rect + vite project, creating homepage, navbar component and landingpage component.

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+import '../App.css';
+export const LandingPage = () => {
+    return(
+        <>
+        <div className="bg-cover bg-center  w-screen h-[91.5vh]  bg-[url('./assets/navbar-image.jpg')]">
+            
+        </div>
+        </>
+    )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import '../App.css';
+import { LandingPage } from '../components/LandingPage';
+import { NavBar } from '../components/NavBar';
+
+export const HomePage = () => {
+    return(
+        <>
+        <NavBar/>
+        <LandingPage/>
+        </>
+    )
+}
