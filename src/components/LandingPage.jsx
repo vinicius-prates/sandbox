b83f41f5bbbfbf10b670c2e0e30a2bc52f2c3d46
vinicius-prates <viniprates2211@gmail.com>
2022-09-13 11:28:16 -0300

    fixing the div of sign in and login

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,13 +3,17 @@ export const LandingPage = () => {
     return(
         <>
         <div className="bg-cover bg-center  w-screen h-[91.5vh]  bg-[url('./assets/navbar-image.jpg')]">
-            <div className="flex flex-col w-64 ">
-                <h1 className="">Welcome to Exo!
+            <div className="flex flex-col w-full justify-center self-center pt-64">
+                <h1 className="text-white font-bold text-2xl self-center text-center justify-center">Welcome to Exo!
                     New here? Create an account.
                 </h1>
-                <div className="my-8    ">
-                    <a className="bg-white text-black font-bold p-4 rounded-[15px]" >Sign In</a>
-                    <a className="bg-blue-600 text-white font-bold p-4 rounded-[15px]"> Log In </a>
+                <div className="my-8 flex gap-6 self-center">
+                    <a className="bg-white text-black font-bold p-4 rounded-[20px] w-32 text-center
+                    hover:bg-black hover:text-white ease-in-out duration-300"
+                    href="" >Sign In</a>
+                    <a className="bg-blue-600 text-white font-bold p-4 rounded-[20px] w-32 text-center
+                    hover:bg-white hover:text-blue-600 ease-in-out duration-300"
+                    href=""> Log In </a>
                 </div>
                     
             </div>
