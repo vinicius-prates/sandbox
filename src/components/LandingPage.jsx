95dd4b3bbef70f0582a88aa9feb6ad6468b1cc25
vinicius-prates <viniprates2211@gmail.com>
2022-09-13 10:34:30 -0300

    adding sign in and log in div at landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,7 +3,16 @@ export const LandingPage = () => {
     return(
         <>
         <div className="bg-cover bg-center  w-screen h-[91.5vh]  bg-[url('./assets/navbar-image.jpg')]">
-            
+            <div className="flex flex-col w-64 ">
+                <h1 className="">Welcome to Exo!
+                    New here? Create an account.
+                </h1>
+                <div className="my-8    ">
+                    <a className="bg-white text-black font-bold p-4 rounded-[15px]" >Sign In</a>
+                    <a className="bg-blue-600 text-white font-bold p-4 rounded-[15px]"> Log In </a>
+                </div>
+                    
+            </div>
         </div>
         </>
     )
