4761737cd620ce9e172e17e7d7db161c1df5f6ea
vinicius-prates <viniprates2211@gmail.com>
2022-09-20 14:44:05 -0300

    finished For You Component

--- src/components/ForYou.jsx
+++ src/components/ForYou.jsx
@@ -5,9 +5,12 @@ import  Cartoes  from '../assets/cartoes.png';
 export const ForYou = () => {
 
     return(
-        <div className="bg-[#161616] bg-cover ">
+        <div className="bg-[#161616] bg-cover text-center flex flex-col gap-4 py-8 px-6 ">
+
+            <h1 className="text-white text-2xl">For You</h1>
             <img src={Cartoes} className="w-full p-4 "></img>
-            <h1 className="text-white">asijasi</h1>
+            <p className="text-white lg:text-right font-thin tracking-wide">Cards for every option you want!</p>
+            <a className="text-white border-b-2 w-[33%] self-center">Card Types -{'>'} </a>
         </div>
     )
 }
