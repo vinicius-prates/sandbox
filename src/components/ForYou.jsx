952deeed005a732d5aab3d1d8eb3fc07ab76fd2b
vinicius-prates <viniprates2211@gmail.com>
2022-09-20 15:00:30 -0300

    added responsivity to For You Component

--- src/components/ForYou.jsx
+++ src/components/ForYou.jsx
@@ -5,12 +5,19 @@ import  Cartoes  from '../assets/cartoes.png';
 export const ForYou = () => {
 
     return(
-        <div className="bg-[#161616] bg-cover text-center flex flex-col gap-4 py-8 px-6 ">
+        <div className="bg-[#161616] bg-cover text-center flex flex-col gap-4 py-8 px-6 justify-center">
 
-            <h1 className="text-white text-2xl">For You</h1>
-            <img src={Cartoes} className="w-full p-4 "></img>
-            <p className="text-white lg:text-right font-thin tracking-wide">Cards for every option you want!</p>
-            <a className="text-white border-b-2 w-[33%] self-center">Card Types -{'>'} </a>
+            <h1 className="text-white text-2xl font-bold">For You</h1>
+            <div className="flex flex-col lg:flex-row justify-center gap-20">
+            <div className=" flex flex-col lg:flex-col gap-5 align-center justify-center ">
+            <p className="text-white lg:text-right font-thin tracking-wide text-xl">Cards for every option you want!</p>
+            <a className="text-white   text-left border-b-2 w-[33%]" 
+                href=''>Card Types -{'>'} </a>
+            </div>
+            <img src={Cartoes} className="w-full p-4 lg:w-[30%]  "></img>
+            
+            
+            </div>
         </div>
     )
 }
