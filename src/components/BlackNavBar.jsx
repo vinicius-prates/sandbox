e673eac9769ea2c50da53ba516b9dae688156054
vinicius-prates <viniprates2211@gmail.com>
2022-09-22 15:57:22 -0300

    added a black navbar to other option

---
+++ src/components/BlackNavBar.jsx
@@ -0,0 +1,77 @@
+import '../App.css';
+import { Link } from 'react-router-dom'
+
+export const BlackNavBar = () => {
+   
+
+    const toggleMenu = () => {
+        const  menu = document.querySelector(".mobile-menu");
+        menu.classList.toggle("hidden");
+    }
+
+    return(
+        
+      
+        <div >
+			   <nav className="relative px-4 py-4 flex justify-between items-center bg-[#252525] w-full">
+		<Link to="/"  className="text-3xl text-white font-bold leading-none  select-none">
+			EXOBANK
+		</Link>
+		<div  className="lg:hidden">
+			<button onClick={toggleMenu}  className="navbar-burger flex items-center text-white p-2 ">
+				<svg  className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<title>Mobile menu</title>
+					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+				</svg>
+			</button>
+		</div>
+		<ul  className="mobile-menu lg:mt-0 mt-[25rem] ml-0 bg-white w-[90%]  h-[40rem]	justify-center
+		 rounded-lg flex-col gap-6 flex md:hidden  md:ml-0 md:bg-transparent text-center  hidden absolute top-1/2 left-1/2 
+		 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto  lg:items-center lg:w-auto lg:space-x-6 
+		 lg:gap-0 lg:flex-row lg:h-0  ">
+			<li><Link to="/"  className="text-lg lg:text-sm text-gray-800 font-bold hover:text-gray-500 border-b-2  select-none" >Home</Link></li>
+		
+			<li><Link to="#aboutexo"  className="text-lg lg:text-sm text-gray-800 hover:text-gray-500 font-bold border-b-2 duration-300 select-none" >About Exo</Link></li>
+			
+			<li><a  className="text-lg lg:text-sm text-gray-800 hover:text-gray-500 font-bold border-b-2  duration-300 select-none" href="#">Services</a></li>
+		
+			<li><a  className="text-lg lg:text-sm text-gray-800 hover:text-gray-500 font-bold border-b-2 duration-300  select-none" href="#">For you</a></li>
+			
+			<li><a  className="text-lg lg:text-sm text-gray-800 hover:text-gray-500 font-bold border-b-2 duration-300  select-none" href="#">Contact</a></li>
+		</ul>
+		<a  className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Sign In</a>
+		<a  className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="#">Sign up</a>
+	</nav>
+	<div  className="navbar-menu relative z-50 hidden">
+		<div  className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+		<nav  className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+			<div  className="flex items-center mb-8">
+				<a   className="mr-auto text-3xl font-bold leading-none" href="#">
+					<svg  className="h-12" alt="logo" viewBox="0 0 100 100">
+						<path d="M100 34.2c-.4-2.6-3.3-4-5.3-5.3-3.6-2.4-7.1-4.7-10.7-7.1-8.5-5.7-17.1-11.4-25.6-17.1-2-1.3-4-2.7-6-4-1.4-1-3.3-1-4.8 0-5.7 3.8-11.5 7.7-17.2 11.5L5.2 29C3 30.4.1 31.8 0 34.8c-.1 3.3 0 6.7 0 10v16c0 2.9-.6 6.3 2.1 8.1 6.4 4.4 12.9 8.6 19.4 12.9 8 5.3 16 10.7 24 16 2.2 1.5 4.4 3.1 7.1 1.3 2.3-1.5 4.5-3 6.8-4.5 8.9-5.9 17.8-11.9 26.7-17.8l9.9-6.6c.6-.4 1.3-.8 1.9-1.3 1.4-1 2-2.4 2-4.1V37.3c.1-1.1.2-2.1.1-3.1 0-.1 0 .2 0 0zM54.3 12.3L88 34.8 73 44.9 54.3 32.4V12.3zm-8.6 0v20L27.1 44.8 12 34.8l33.7-22.5zM8.6 42.8L19.3 50 8.6 57.2V42.8zm37.1 44.9L12 65.2l15-10.1 18.6 12.5v20.1zM50 60.2L34.8 50 50 39.8 65.2 50 50 60.2zm4.3 27.5v-20l18.6-12.5 15 10.1-33.6 22.4zm37.1-30.5L80.7 50l10.8-7.2-.1 14.4z"></path>
+					</svg>
+				</a>
+				<button  className="navbar-close">
+					<svg  className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+					</svg>
+				</button>
+			</div>
+			
+			<div  className="mt-auto">
+				<div  className="pt-6">
+					<a  className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl" href="#">Sign in</a>
+					<a  className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl" href="#">Sign Up</a>
+				</div>
+				<p  className="my-4 text-xs text-center text-gray-400">
+					<span>Copyright © 2021</span>
+				</p>
+			</div>
+		</nav>
+	</div>
+
+		</div>
+    
+       
+    )
+}
--- src/pages/CardsPage.jsx
+++ src/pages/CardsPage.jsx
@@ -1,6 +1,6 @@
 import '../App.css';
 import { Link } from 'react-router-dom'
-import { NavBar } from '../components/NavBar';
+import { BlackNavBar } from '../components/BlackNavBar';
 import { Footer } from '../components/Footer';
 import  GoldCard  from '../assets/card.png';
 import ExclusiveCard from '../assets/card2.png';
@@ -8,8 +8,8 @@ import PlatinumCard from '../assets/card3.png';
 export const CardsPage = () => {
     return(
         <>
-        <NavBar/>
-        <div className="bg-[#252525] flex flex-col text-center py-12">
+        <BlackNavBar/>
+        <div className="bg-[#252525] flex flex-col text-center py-12 gap-8">
             <h1 className="text-white text-4xl">3 Types of Cards!</h1>
             <div >
                 <img src={GoldCard} className="bg-[#252525]"></img>
