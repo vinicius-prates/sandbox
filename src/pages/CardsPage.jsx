62471cabc11408dbcd17f8401e9774a904c87373
--replace-all <viniprates2211@gmail.com>
2022-09-23 10:53:05 -0300

    added responsivity to CardsPage, And fixed BlackNavBar Bug

--- src/pages/CardsPage.jsx
+++ src/pages/CardsPage.jsx
@@ -40,31 +40,38 @@ export const CardsPage = () => {
     return(
         <>
         <BlackNavBar/>
-        <h1 className="text-white bg-[#252525] text-center py-2 text-4xl">3 Types of Cards!</h1>
+        <h1 className="text-white bg-[#252525] text-center py-2 text-4xl justify-center ">3 Types of Cards!</h1>
 
-        <div className="bg-[#252525] flex flex-col text-center py-12 gap-8 lg:flex-row">
+        <div className="bg-[#252525] grid flex-col text-center py-12 gap-12 lg:grid-flow-col lg:grid-rows-2 lg:px-96">
 
-            <div className="flex flex-col ">
-                <img src={GoldCard} className="bg-[#252525] "></img>
-                <button className="button text-white text-sm text-left mx-5 border-b-2 w-[16.5%] " onClick={show_details_gold}>More Info</button>
+            <div className="flex flex-col lg:w-[30rem]">
+                <img src={GoldCard} className="bg-[#252525]  "></img>
+                <button className="button text-white text-sm text-left mx-5 border-b-2 w-[16.4%] " onClick={show_details_gold}>More Info</button>
                 <div id="dropDown" className="hidden">
                     <p className=" text-white text-xs text-left m-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in bibendum sapien. Quisque et enim vel dolor vulputate pharetra in varius mauris. Integer a leo id elit rutrum finibus ac id purus. Praesent eu nunc faucibus, iaculis erat eu, fermentum eros. Aliquam eleifend scelerisque ante quis gravida. Suspendisse pharetra tortor eu rhoncus tempus. Nam sagittis mas</p>
                 </div>
                 
             </div>
 
-            <div className="flex flex-col">
+            <div className="flex flex-col lg:w-[30rem]">
                 <img src={ExclusiveCard} className=" bg-[#252525] "></img>
-                <button className="text-white text-sm text-left mx-5 border-b-2 w-[16.5%]" onClick={show_details_exclusive}>More Info</button>
+                <button className="text-white text-sm text-left mx-5 border-b-2 w-[16.4%]" onClick={show_details_exclusive}>More Info</button>
                 <div id="dropDown2" className="hidden">
                     <p className=" text-white text-xs text-left m-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in bibendum sapien. Quisque et enim vel dolor vulputate pharetra in varius mauris. Integer a leo id elit rutrum finibus ac id purus. Praesent eu nunc faucibus, iaculis erat eu, fermentum eros. Aliquam eleifend scelerisque ante quis gravida. Suspendisse pharetra tortor eu rhoncus tempus. Nam sagittis mas</p>
                 </div>
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col lg:w-[30rem]">
                 <img src={PlatinumCard} className="bg-[#252525]  "></img>
-                <button className="text-white text-sm text-left mx-5 border-b-2 w-[16.5%]" onClick={show_details_platinum}>More info</button>
+                <button className="text-white text-sm text-left mx-5 border-b-2 w-[16.4%]" onClick={show_details_platinum}>More info</button>
                 <div id="dropDown3" className="hidden ">
                     <p className=" text-white text-xs text-left m-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in bibendum sapien. Quisque et enim vel dolor vulputate pharetra in varius mauris. Integer a leo id elit rutrum finibus ac id purus. Praesent eu nunc faucibus, iaculis erat eu, fermentum eros. Aliquam eleifend scelerisque ante quis gravida. Suspendisse pharetra tortor eu rhoncus tempus. Nam sagittis mas</p></div>
+            </div>
+            <div className="flex flex-col mx-6 my-4 text-left gap-2     lg:w-[25rem]">
+                <h1 className="text-white text-3xl">What do you think?</h1>
+                <p className="text-white text-lg text-left">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at turpis eleifend,
+                     dignissim ipsum eu, rutrum neque. In vehicula rhoncus interdum. </p>
+                <a className="text-white text-lg font-bold py-6 px-2 bg-blue-700  rounded-2xl transition duration-200 text-center hover:bg-white hover:text-blue-600" href=""> Register!</a>
             </div>
         </div>
         <Footer/>
