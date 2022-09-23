b0d1205f86451405e1c2f7d1ad308e6209535993
vinicius-prates <viniprates2211@gmail.com>
2022-09-23 16:19:34 -0300

    changed the form in the register page, and connect to the back-end.

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -8,23 +8,29 @@ import axios from 'axios';
 export const RegisterPage = () => {
 
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
     const [cpf, setCPF] = useState("");
     const [password, setPassword] = useState("");
-    const [birthDate, setBirthDate] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    const [data, getAllData]  = useState("");
-    const url = 'http://localhost:8000/'
-    const  getData = async () => {
-        try {
-            const {data} = await axios.get(`${url}api/client/`)
-            console.log(data)
-        }catch(err){
-            console.log(err)
-        }
-        
+    const url = 'http://localhost:8000/api/user/'
+
+    var  data = {
+        cpf: cpf,
+        password: password
     }
+    const saveUserRegister = () => {
+        event.preventDefault()
+        if(password == confirmPassword && password != "" ){
+
+            axios.post(url, data)
+        } else {
+            console.log("senha inválida!")
+        }
+    }
+
+    
+
+
     return(
         <div>
             <NavBar/>
@@ -33,16 +39,16 @@ export const RegisterPage = () => {
                 <div className="flex flex-col text-center items-center gap-10  bg-gray-100 p-6 mx-6 p rounded-lg w-[42rem] lg:h-[24rem] shadow-xl shadow-gray-300">
                     <h1 className="font-bold text-3xl ">First time here?</h1>
                     <div className="flex lg:flex-row flex-col gap-8">
-                    <div className="flex flex-col gap-4">
-                        <input placeholder="CPF" type="text" className="p-1 border-b-2 focus:outline-none"></input>
-                        <input placeholder="Password" type="password" className="p-1 border-b-2 focus:outline-none"></input>
-                        <input placeholder="Confirm Password" type="password" className="p-1 border-b-2 focus:outline-none"></input>
+                    <form className="flex flex-col gap-4" onSubmit={saveUserRegister}>
+                        <input placeholder="CPF" type="text" value={cpf} onChange={(evt) =>{setCPF(evt.target.value)}} className="p-1 border-b-2 focus:outline-none"></input>
+                        <input placeholder="Password" type="password" value={password} onChange={(evt) => {setPassword(evt.target.value)}} className="p-1 border-b-2 focus:outline-none"></input>
+                        <input placeholder="Confirm Passpword" type="password" value={confirmPassword} onChange={(evt) => {setConfirmPassword(evt.target.value)}} className="p-1 border-b-2 focus:outline-none"></input>
+                        <button className="bg-blue-600 text-white font-bold p-4 rounded-[20px] w-52  text-center
+                        hover:bg-white hover:text-blue-600 ease-in-out duration-300 cursor-pointer">Register</button>
+                    </form>
 
                     </div>
                     
-                    </div>
-                    <a className="bg-blue-600 text-white font-bold p-4 rounded-[20px] w-52  text-center
-                        hover:bg-white hover:text-blue-600 ease-in-out duration-300 cursor-pointer">Register</a>
                         <div className="flex gap-1 lg:mt-[-30px] lg:m-2">
                             <p>Already have a login?</p>
                         <Link to="/login" className="text-blue-600 hover:text-blue-400 transition">Login</Link>
