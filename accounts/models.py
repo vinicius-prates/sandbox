8ca40d20ff491a9c9456d8ccb1923373084b37b9
vinicius-prates <viniprates2211@gmail.com>
2022-09-27 08:43:13 -0300

    changed the User models, CPF is the primary_key

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, primary_key=True)
     password = models.CharField(max_length=100)
     
 
