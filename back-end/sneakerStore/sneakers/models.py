27563b79cd704631b8547e46815d7dd96da701f5
vinicius-prates <viniprates2211@gmail.com>
2022-10-31 16:59:33 -0300

    NEW: started creating the models.

--- back-end/sneakerStore/sneakers/models.py
+++ back-end/sneakerStore/sneakers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
-# Create your models here.
+class SneakerSize(models.Model):
+    sneaker_size = models.IntegerField(default=39,  validators=[MinValueValidator(20), MaxValueValidator(50)])
+class User (models.Model):
+
+    nickname = models.CharField(max_length=100)
+    email = models.CharField(max_length=255)
+    sneaker_size = models.ForeignKey(SneakerSize, on_delete= models.DO_NOTHING)
+
+
