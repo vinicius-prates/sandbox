23981ff76ee6bf9b47d580d8e953cd7be142edda
vinicius-prates <viniprates2211@gmail.com>
2022-11-01 08:39:25 -0300

    NEW: Models, Serializers and ViewSets done. Add all to router, API working

--- back-end/sneakerStore/sneakers/urls.py
+++ back-end/sneakerStore/sneakers/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
 
 from rest_framework import routers
+from .views import (UserViewSet,ConditionViewSet,BrandViewSet,SneakerSizeViewSet,SneakerViewSet )
+
+
 
 router = routers.DefaultRouter()
-
+router.register("users", UserViewSet)
+router.register("conditions", ConditionViewSet)
+router.register("brands", BrandViewSet)
+router.register("sneakers-sizes",SneakerSizeViewSet)
+router.register("sneakers", SneakerViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
--- back-end/sneakerStore/sneakers/views.py
+++ back-end/sneakerStore/sneakers/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import (Brand, Condition,Sneaker, SneakerSize, User)
+from rest_framework.viewsets import ModelViewSet
+
+from .serializers import (ConditionSerializer,BrandSerializer,SneakerSerializer,SneakerSizeSerializer,UserSerializer)
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ConditionViewSet(ModelViewSet):
+    queryset = Condition.objects.all()
+    serializer_class = ConditionSerializer
+
+class BrandViewSet(ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+class SneakerSizeViewSet(ModelViewSet):
+    queryset = SneakerSize.objects.all()
+    serializer_class = SneakerSizeSerializer
+    
+class SneakerViewSet(ModelViewSet):
+    queryset = Sneaker.objects.all()
+    serializer_class = SneakerSerializer
