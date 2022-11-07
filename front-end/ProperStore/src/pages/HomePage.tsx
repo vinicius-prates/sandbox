20504ecaf4d7e175bb792ab4019ecc8eb79c5d8a
vinicius-prates <viniprates2211@gmail.com>
2022-11-07 10:31:52 -0300

    NEW: added slide show

--- front-end/ProperStore/src/pages/HomePage.tsx
+++ front-end/ProperStore/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { NavBar } from "../components/NavBar"
+import { SlideShow } from "../components/SlideShow"
 
 export const HomePage = () => {
     return(
         <div>
             <NavBar/>
+            <SlideShow/>
         </div>
     )
 }
