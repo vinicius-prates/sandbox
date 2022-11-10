4ebc04680b8d9c02bc8811b7888b013765be7b48
vinicius-prates <viniprates2211@gmail.com>
2022-11-10 16:22:34 -0300

    added tailwind and react router to the project

---
+++ front-end/src/MyRoutes.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom"
+import App from "./App"
+
+export const MyRoutes = () => {
+    return(
+        <Routes>
+            <Route path="/" element={<App/>}/>
+        </Routes>
+    )
+}
