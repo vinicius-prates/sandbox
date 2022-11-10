1c089955535d909e5524a72edda5f9feda1aed2a
vinicius-prates <viniprates2211@gmail.com>
2022-11-10 17:00:07 -0300

    FEATURE: added navbar

--- front-end/src/MyRoutes.tsx
+++ front-end/src/MyRoutes.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom"
-import App from "./App"
+import { HomePage } from "./HomePage"
 
 export const MyRoutes = () => {
     return(
         <Routes>
-            <Route path="/" element={<App/>}/>
+            <Route path="/" element={<HomePage/>}/>
         </Routes>
     )
 }
---
+++ front-end/src/components/NavBar.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const NavBar = () => {
+    const [navbar, setNavbar] = useState(false);
+
+    return(
+        <nav className="w-full  bg-[#161616] shadow">
+        <div className="justify-between px-4 mx-auto lg:max-w-8xl md:items-center md:flex md:px-8">
+            <div>
+                <div className="flex items-center justify-between pb-8 pt-10 md:py-10 md:block">
+                    <Link to="/">
+                        <h2 className="text-2xl text-[#FAF9F6] focus:outline-none font-bold">EXOBANK</h2>
+                        </Link>
+                    <div className="md:hidden">
+                        <button
+                            className="p-2 text-[#FAF9F6] rounded-md outline-none focus:border-gray-400 focus:outline-none"
+                            onClick={() => setNavbar(!navbar)}
+                        >
+                            {navbar ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-6 h-6"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="w-6 h-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div
+                    className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0  ${
+                        navbar ? "block" : "hidden"
+                    }`}
+                >
+                    <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                        <li className="text-[#FAF9F6] ">
+                            <Link to="/addnew-sneaker">For you</Link>
+                        </li>
+                        <li className="text-[#FAF9F6]">
+                            <Link to="/all-sneakers">Cards</Link>
+                        </li>
+                        <li className="text-[#161616] bg-[#FAF9F6] px-4 py-2 rounded-lg text-lg font-bold hover:text-[#FAF9F6] hover:bg-[#323232] transition-all">
+                            <a href="">Login</a>
+                        </li>
+                        <li className="text-[#FAF9F6] text-lg font-bold bg-blue-700 px-4 py-2 rounded-lg hover:text-blue-700 hover:bg-[#faf9f6]  transition-all">
+                            <a href="">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+);
+}
+    
