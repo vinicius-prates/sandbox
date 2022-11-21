0e168b1c8f947ecdd20dc8b2b58e6845db9a6e4e
vinicius-prates <viniprates2211@gmail.com>
2022-11-21 08:32:25 -0300

    FIX: error 404 at client image

--- banco_teste/settings.py
+++ banco_teste/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -109,6 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
